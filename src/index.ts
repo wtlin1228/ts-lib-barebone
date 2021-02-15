55d2a5e1c438b2d1f0cdc5d59435c0781f0121e7
wtlin <wtlin1228@gmail.com>
2021-02-15 14:57:59 +0800

    style: add prettier

--- src/index.ts
+++ src/index.ts
@@ -12,31 +12,31 @@
  * @public
  */
 export function avg(a: number, b: number, c: number): number {
-    return sum3(a, b, c) / 3;
-  }
-  
-  /**
-   * Calculate the sum of three numbers
-   *
-   * @param a - first number
-   * @param b - second number
-   * @param c - third number
-   *
-   * @beta
-   */
-  export function sum3(a: number, b: number, c: number): number {
-    return sum2(a, sum2(b, c));
-  }
-  
-  /**
-   * Calculate the sum of two numbers
-   *
-   * @param a - first number
-   * @param b - second number
-   *
-   * @internal
-   */
-  export function sum2(a: number, b: number): number {
-    const sum = a + b;
-    return sum;
-  }
+  return sum3(a, b, c) / 3;
+}
+
+/**
+ * Calculate the sum of three numbers
+ *
+ * @param a - first number
+ * @param b - second number
+ * @param c - third number
+ *
+ * @beta
+ */
+export function sum3(a: number, b: number, c: number): number {
+  return sum2(a, sum2(b, c));
+}
+
+/**
+ * Calculate the sum of two numbers
+ *
+ * @param a - first number
+ * @param b - second number
+ *
+ * @internal
+ */
+export function sum2(a: number, b: number): number {
+  const sum = a + b;
+  return sum;
+}
